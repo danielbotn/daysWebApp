2ef3573b1ae92bab62bf731b7c48bb436e6d81e8
danielfrs87 <daniels@re.is>
2021-01-01 11:41:29 +0000

    :sparkles: Added new component and lint config

---
+++ src/components/SuccessPage/SuccessPage.svelte
@@ -0,0 +1,41 @@
+<style>
+  i {
+    font-size: 100px;
+    line-height: 200px;
+    margin-left:-15px;
+    padding-left: 60px;
+    @apply text-green-500
+  }
+  .card {
+    background: white;
+    padding: 60px;
+    border-radius: 4px;
+    box-shadow: 0 2px 3px #C8D0D8;
+    display: inline-block;
+    margin: 0 auto;
+  }
+</style>
+
+<script lang="ts">
+  export let email: string;
+</script>
+
+<div class="bg-grey-lighter min-h-screen flex flex-col">
+  <div class="container max-w-md mx-auto flex-1 flex flex-col items-center justify-center px-2">
+      <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+          <h1 class="mb-8 text-3xl text-center text-green-500">Success</h1>
+          <div style="border-radius:200px; height:200px; width:200px; background: #F8FAF5; margin:0 auto;">
+            <i class="checkmark">✓</i>
+          </div>
+          <div class="text-center text-sm text-blue-500 mt-4">
+            <p>Thank you for signing up with Days</p>
+            <p class="no-underline border-b border-grey-dark text-grey-dark">
+                Please check your email to confirm registration <br />
+            </p>
+            {#if email}
+             @ {email}
+            {/if}
+        </div>
+      </div>
+  </div>
+</div>
--- src/routes/register/index.svelte
+++ src/routes/register/index.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
     import * as EmailValidator from 'email-validator';
     import Spinner from '../../components/Svgs/Spinner.svelte';
+    import SuccessPage from '../../components/SuccessPage/SuccessPage.svelte';
 
-    let loginPressed : boolean = false;
+    let regButtonPressed: boolean = false;
+    let registerSuccessfull: boolean = false;
     let firstName = '', lastName = '', email = '', password = '';
     let firstNameError = false, lastNameError = false, emailError = false, passwordError = false;
 
@@ -29,8 +31,10 @@
         }
     };
     const register = () => {
+        regButtonPressed = true;
         console.log('you just clicked the register button');
         validation();
+        registerSuccessfull = true;
     };
 </script>
 
@@ -44,67 +48,80 @@
     }
 
 </style>
-<div class="bg-grey-lighter min-h-screen flex flex-col">
-  <div class="container max-w-md mx-auto flex-1 flex flex-col items-center justify-center px-2">
-      <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
-          <h1 class="mb-8 text-3xl text-center">Sign up</h1>
-          {#if firstNameError}
-            <span class="text-red-500 text-xs italic">Please enter first name</span>
-           {/if}
-          <input 
-            type="text"
-            name="fullname"
-            placeholder="First Name"
-            bind:value="{firstName}"
-          />
-          {#if lastNameError}
-            <span class="text-red-500 text-xs italic">Please enter last name</span>
-           {/if}
-          <input 
-            type="text"
-            name="fullname"
-            placeholder="Last Name"
-            bind:value="{lastName}"
-         />
-          {#if emailError}
-            <span class="text-red-500 text-xs italic">Please enter valid email</span>
-          {/if}
-          <input 
-            type="text"
-            name="Email"
-            placeholder="Email"
-            id="email"
-            autocomplete="off"
-            bind:value="{email}"
-          />
-          {#if passwordError}
-            <span class="text-red-500 text-xs italic">Please enter valid password</span>
-          {/if}
-          <input 
-            type="password"
-            name="Password"
-            id="password"
-            placeholder="Password"
-            autocomplete="new-password"
-            bind:value="{password}"
-          />
-          <button type="submit" on:click={register}>Create Account</button>
 
-          <div class="text-center text-sm text-grey-dark mt-4">
-              By signing up, you agree to the 
-              <a class="no-underline border-b border-grey-dark text-grey-dark" href="#">
-                  Terms of Service
-              </a> and 
-              <a class="no-underline border-b border-grey-dark text-grey-dark" href="#">
-                  Privacy Policy
-              </a>
-          </div>
+{#if !registerSuccessfull}
+    <div class="bg-grey-lighter min-h-screen flex flex-col">
+        <div class="container max-w-md mx-auto flex-1 flex flex-col items-center justify-center px-2">
+        <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+            <h1 class="mb-8 text-3xl text-center">Sign up</h1>
+            {#if firstNameError}
+              <span class="text-red-500 text-xs italic">Please enter first name</span>
+             {/if}
+            <input 
+              type="text"
+              name="fullname"
+              placeholder="First Name"
+              bind:value="{firstName}"
+            />
+            {#if lastNameError}
+              <span class="text-red-500 text-xs italic">Please enter last name</span>
+             {/if}
+            <input 
+              type="text"
+              name="fullname"
+              placeholder="Last Name"
+              bind:value="{lastName}"
+           />
+            {#if emailError}
+              <span class="text-red-500 text-xs italic">Please enter valid email</span>
+            {/if}
+            <input 
+              type="text"
+              name="Email"
+              placeholder="Email"
+              id="email"
+              autocomplete="off"
+              bind:value="{email}"
+            />
+            {#if passwordError}
+              <span class="text-red-500 text-xs italic">Please enter valid password</span>
+            {/if}
+            <input 
+              type="password"
+              name="Password"
+              id="password"
+              placeholder="Password"
+              autocomplete="new-password"
+              bind:value="{password}"
+            />
+            <button on:click="{register}">
+              {#if regButtonPressed}
+                <Spinner />
+              {:else}
+                Create Account
+              {/if }
+             </button>
+  
+            <div class="text-center text-sm text-grey-dark mt-4">
+                By signing up, you agree to the 
+                <a class="no-underline border-b border-grey-dark text-grey-dark" href="#">
+                    Terms of Service
+                </a> and 
+                <a class="no-underline border-b border-grey-dark text-grey-dark" href="#">
+                    Privacy Policy
+                </a>
+            </div>
+        </div>
+        <div class="text-center text-sm text-blue-500 mt-4">
+          Already have an account? 
+          <a class="no-underline border-b border-grey-dark text-blue-500" href="/">
+              Log in
+          </a>
       </div>
-      <div class="text-center text-sm text-blue-500 mt-4">
-        Already have an account? 
-        <a class="no-underline border-b border-grey-dark text-blue-500" href="/">
-            Log in
-        </a>
     </div>
   </div>
-</div>
+{/if}
+
+{#if registerSuccessfull }
+    <SuccessPage email={email} />
+{/if}
